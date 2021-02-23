2d851057d3975f38101820cb98789b2c0a7d02db
Hung <phantrankhanhhung@gmail.com>
2021-02-23 13:04:25 +0700

    Da test xong

--- js/viewButton.js
+++ js/viewButton.js
@@ -2,7 +2,7 @@ let item = document.getElementsByClassName("viewMd");
 let viewLessbtn = document.getElementById("viewLess");
 console.log(viewLessbtn);
 let viewAllbtn = document.getElementById("viewAll");
-
+let sceenSize = sceen.width;
 function viewAll() {
     for (var i = 0; i < item.length; i++) {
         item[i].style.display = "block";
@@ -22,7 +22,8 @@ function viewLess() {
 
 function reSizeWidown() {
     let windowSize = document.documentElement.clientWidth;
-    if (parseFloat(windowSize) > 768) {
+    let sceenSize = sceen.width;
+    if (parseFloat(windowSize) > 768 || parseFloat(sceenSize) > 768) {
         if (viewAllbtn.style.display === "block" || viewLessbtn.style.display === "block") {
             for (var i = 0; i < item.length; i++) {
                 item[i].style.display = "block";
@@ -31,7 +32,7 @@ function reSizeWidown() {
             viewLessbtn.style.display = "none";
         }
       }
-      if (parseInt(windowSize) <= 768) {
+      if (parseInt(windowSize) <= 768 || parseFloat(sceenSize) <= 768) {
         for (var i = 0; i < item.length; i++) {
             item[i].style.display = "none";
         }
