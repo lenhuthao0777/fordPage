adb0b27fecb768c39f8b99b1de88c7056116ca46
Hung <phantrankhanhhung@gmail.com>
2021-02-23 08:09:58 +0700

    Da sua loi

--- js/viewButton.js
+++ js/viewButton.js
@@ -18,3 +18,24 @@ function viewLess() {
         viewAllbtn.style.display = "block";
     }
 }
+
+
+function reSizeWidown() {
+    let windowSize = document.documentElement.clientWidth;
+    if (parseFloat(windowSize) > 768) {
+        if (viewAllbtn.style.display === "block" || viewLessbtn.style.display === "block") {
+            for (var i = 0; i < item.length; i++) {
+                item[i].style.display = "block";
+            }
+            viewAllbtn.style.display = "none";
+            viewLessbtn.style.display = "none";
+        }
+      }
+      if (parseInt(windowSize) <= 768) {
+        for (var i = 0; i < item.length; i++) {
+            item[i].style.display = "none";
+        }
+        viewAllbtn.style.display = "block";
+        viewLessbtn.style.display = "none";
+  }
+}
