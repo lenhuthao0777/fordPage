607ade85770c291d5125abd79f4e7cb47a6ee679
Hung <phantrankhanhhung@gmail.com>
2021-02-23 13:44:08 +0700

    Da test xong

--- js/viewButton.js
+++ js/viewButton.js
@@ -2,28 +2,35 @@ let item = document.getElementsByClassName("viewMd");
 let viewLessbtn = document.getElementById("viewLess");
 console.log(viewLessbtn);
 let viewAllbtn = document.getElementById("viewAll");
-let sceenSize = sceen.width;
+
 function viewAll() {
-    for (var i = 0; i < item.length; i++) {
-        item[i].style.display = "block";
-        viewAllbtn.style.display = "none";
-        viewLessbtn.style.display = "block";
+    let sceenSize = document.documentElement.clientWidth;
+    if (sceenSize <= 768) {
+        for (var i = 0; i < item.length; i++) {
+            item[i].style.display = "block";
+            viewAllbtn.style.display = "none";
+            viewLessbtn.style.display = "block";
+        }
     }
 }
 
 function viewLess() {
-    for (var i = 0; i < item.length; i++) {
-        item[i].style.display = "none";
+    let sceenSize = document.documentElement.clientWidth;
+    if (sceenSize <= 768) {
+        for (var i = 0; i < item.length; i++) {
+            item[i].style.display = "none";
+            viewLessbtn.style.display = "none";
+            viewAllbtn.style.display = "block";
+        }
+    } else {
         viewLessbtn.style.display = "none";
-        viewAllbtn.style.display = "block";
     }
 }
 
 
 function reSizeWidown() {
     let windowSize = document.documentElement.clientWidth;
-    let sceenSize = sceen.width;
-    if (parseFloat(windowSize) > 768 || parseFloat(sceenSize) > 768) {
+    if (parseFloat(windowSize) > 768) {
         if (viewAllbtn.style.display === "block" || viewLessbtn.style.display === "block") {
             for (var i = 0; i < item.length; i++) {
                 item[i].style.display = "block";
@@ -32,7 +39,7 @@ function reSizeWidown() {
             viewLessbtn.style.display = "none";
         }
       }
-      if (parseInt(windowSize) <= 768 || parseFloat(sceenSize) <= 768) {
+      if (parseInt(windowSize) <= 768) {
         for (var i = 0; i < item.length; i++) {
             item[i].style.display = "none";
         }
