46d2a31e740deb71279194259f3c87f113c837f3
Hung <phantrankhanhhung@gmail.com>
2021-02-27 22:34:45 +0700

    Hoan chinh nut scrollCarousel

--- js/scrollIndicatorBtn.js
+++ js/scrollIndicatorBtn.js
@@ -9,12 +9,24 @@ function scrollToSomething() {
 
 function addDisplayBlock() {
     scrollCarousel.style.display = "block";
+    let sceenSize = document.documentElement.clientWidth;
+    if (sceenSize <= 1200) {
+        scrollCarousel.style.display = "none";
+    }
 }
 
 function addDisplayNone() {
     scrollCarousel.style.display = "none";
 }
+// Hàm xác định kích cỡ màn hình nếu bé hơn 1200px sẽ ẩn nút scrollCarousel
+function isNeedScrollBtn() {
+    let sceenSize = document.documentElement.clientWidth;
+    if (sceenSize <= 1200) {
+        scrollCarousel.style.display = "none";
+    }
+}
 
+// back to top button
 function backToTop() {
     rootElement.scrollTo({
         top: 0
